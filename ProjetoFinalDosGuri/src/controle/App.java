a9cfb2b365300445b9be7653a8b3b8dbd52c7f83
Pedro Dahmer <57972383+cavanhas@users.noreply.github.com>
2019-12-03 23:21:26 -0300

    Update App.java

--- ProjetoFinalDosGuri/src/controle/App.java
+++ ProjetoFinalDosGuri/src/controle/App.java
@@ -3,13 +3,13 @@ package controle;
 import modelo.Fila;
 import modelo.ListaPacientes;
 import modelo.Paciente;
-import vis„o.TelaPrincipal;
+import vis√£o.TelaPrincipal;
 
 public class App {
 	
 	public static void main(String[] args) {
 		
-		//TelaPrincipal t = new TelaPrincipal();
+		TelaPrincipal t = new TelaPrincipal();
 		t.setVisible(true);
 		Paciente p = new Paciente();
 		PacienteControle pcon = new PacienteControle(p, t);
